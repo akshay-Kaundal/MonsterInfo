540070770d269dfd7efa33305a64501ceedaf5fe
akshay <akshay.kaundal@nagarro.com>
2023-01-09 12:26:08 +0530

    checking in formated changes

--- src/app/Module/app-routing.module.ts
+++ src/app/Module/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddmonsterComponent } from '../addmonster/addmonster.component';
 import { HomeComponent } from '../home/home.component';
-import { HomeModule } from './home.module';
-import { MonsterdetailComponent } from '../monsterdetail/monsterdetail.component';
 
 const routes: Routes = [
+  {path:'', component:HomeComponent},
   {path: 'add', component: AddmonsterComponent}, 
   {path: 'home', loadChildren: () => import('./home.module').then(x => x.HomeModule)},
   {path: 'detail/:id', loadChildren: () => import('./monsterdetail.module').then(x => x.MonsterdetailModule)}
